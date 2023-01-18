8ecab1ea115400c2179632861f8ec00974281724
Marek Szulc <235991@edu.p.lodz.pl>
2023-01-18 12:45:45 +0100

    add ingame status

--- src/ListLobbies.java
+++ src/ListLobbies.java
@@ -10,7 +10,11 @@ public class ListLobbies {
     public String getLobbiesString() {
         StringBuilder lobbiesString = new StringBuilder("Lobbies: " + "\n");
         for (int i = 0; i < lobbies.size(); i++) {
-            lobbiesString.append("\nLobby number ").append(i).append("\n");
+            lobbiesString.append("\nLobby number ").append(i);
+            if (lobbies.get(i).gameStarted)
+                lobbiesString.append(" (ingame)");
+            lobbiesString.append(":\n");
+
             for (int j = 0; j < lobbies.get(i).playerNames.length; j++) {
                 lobbiesString.append(j).append(". ");
                 if (lobbies.get(i).playerNames[j] != null)
